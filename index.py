f39f6701ff364f02233a5cac2d0fa34df4090ecf
Pradeep sahu <69186987+theroyal2504@users.noreply.github.com>
2025-02-02 21:02:06 +0530

    Update index.py
    student ditail take for user input

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
+#student ditail are enter  and so  to though give roll number
 
+dict1 ={}
+student_number = int(input("how many student  ditail are inter :"))
+for i in range(student_number):
+    key = input("Enter student roll number  to give you = ")
+    value = {}
+    dict1[key] = value
+    for a in range(2):
+        student_key = input("enter the name,addres,mobile no.,class,mark = ")
+        student_value = input("enter = ")
+        value[student_key] = student_value
+        
+    print("\n\n",value)
+print("\n\n",dict1)
+print("heello")
